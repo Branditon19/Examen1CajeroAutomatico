e9df3bbcaf2077b49e4e6e9e97c182d07fbcca9b
delga <delga@BRAN>
2025-06-03 21:58:28 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace Examen1CajeroAutomatico
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            btnDepositar = new Button();
+            btnRetirar = new Button();
+            btnConsultar = new Button();
+            btnSalir = new Button();
+            btnLimpiar = new Button();
+            txtUsuario = new TextBox();
+            txtmonto = new TextBox();
+            lblSaldoActual = new Label();
+            lstHistorial = new ListBox();
+            lblUsuario = new Label();
+            lblMonto = new Label();
+            lblError = new Label();
+            cmnuMenu = new ContextMenuStrip(components);
+            cambiarColorDeFondoToolStripMenuItem = new ToolStripMenuItem();
+            limpiarHistorialToolStripMenuItem = new ToolStripMenuItem();
+            salirToolStripMenuItem = new ToolStripMenuItem();
+            colorDialog1 = new ColorDialog();
+            cmnuMenu.SuspendLayout();
+            SuspendLayout();
+            // 
+            // btnDepositar
+            // 
+            btnDepositar.Location = new Point(75, 183);
+            btnDepositar.Name = "btnDepositar";
+            btnDepositar.Size = new Size(94, 29);
+            btnDepositar.TabIndex = 0;
+            btnDepositar.Text = "Depositar";
+            btnDepositar.UseVisualStyleBackColor = true;
+            btnDepositar.Click += btnDepositar_Click;
+            // 
+            // btnRetirar
+            // 
+            btnRetirar.Location = new Point(227, 183);
+            btnRetirar.Name = "btnRetirar";
+            btnRetirar.Size = new Size(94, 29);
+            btnRetirar.TabIndex = 1;
+            btnRetirar.Text = "Retirar";
+            btnRetirar.UseVisualStyleBackColor = true;
+            btnRetirar.Click += btnRetirar_Click;
+            // 
+            // btnConsultar
+            // 
+            btnConsultar.Location = new Point(384, 183);
+            btnConsultar.Name = "btnConsultar";
+            btnConsultar.Size = new Size(141, 29);
+            btnConsultar.TabIndex = 2;
+            btnConsultar.Text = "Consultar Saldo";
+            btnConsultar.UseVisualStyleBackColor = true;
+            btnConsultar.Click += btnConsultar_Click;
+            // 
+            // btnSalir
+            // 
+            btnSalir.Location = new Point(685, 407);
+            btnSalir.Name = "btnSalir";
+            btnSalir.Size = new Size(94, 29);
+            btnSalir.TabIndex = 3;
+            btnSalir.Text = "Salir";
+            btnSalir.UseVisualStyleBackColor = true;
+            btnSalir.Click += btnSalir_Click;
+            // 
+            // btnLimpiar
+            // 
+            btnLimpiar.Location = new Point(594, 183);
+            btnLimpiar.Name = "btnLimpiar";
+            btnLimpiar.Size = new Size(94, 29);
+            btnLimpiar.TabIndex = 4;
+            btnLimpiar.Text = "Limpiar";
+            btnLimpiar.UseVisualStyleBackColor = true;
+            btnLimpiar.Click += btnLimpiar_Click;
+            // 
+            // txtUsuario
+            // 
+            txtUsuario.Location = new Point(384, 31);
+            txtUsuario.Name = "txtUsuario";
+            txtUsuario.Size = new Size(130, 27);
+            txtUsuario.TabIndex = 5;
+            // 
+            // txtmonto
+            // 
+            txtmonto.Location = new Point(384, 73);
+            txtmonto.Name = "txtmonto";
+            txtmonto.Size = new Size(130, 27);
+            txtmonto.TabIndex = 6;
+            // 
+            // lblSaldoActual
+            // 
+            lblSaldoActual.AutoSize = true;
+            lblSaldoActual.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblSaldoActual.Location = new Point(319, 120);
+            lblSaldoActual.Name = "lblSaldoActual";
+            lblSaldoActual.Size = new Size(170, 38);
+            lblSaldoActual.TabIndex = 2;
+            lblSaldoActual.Text = "Saldo Actual";
+            lblSaldoActual.Click += lblSaldoActual_Click;
+            // 
+            // lstHistorial
+            // 
+            lstHistorial.FormattingEnabled = true;
+            lstHistorial.Location = new Point(269, 239);
+            lstHistorial.Name = "lstHistorial";
+            lstHistorial.Size = new Size(284, 164);
+            lstHistorial.TabIndex = 8;
+            // 
+            // lblUsuario
+            // 
+            lblUsuario.AutoSize = true;
+            lblUsuario.Location = new Point(319, 38);
+            lblUsuario.Name = "lblUsuario";
+            lblUsuario.Size = new Size(59, 20);
+            lblUsuario.TabIndex = 9;
+            lblUsuario.Text = "Usuario";
+            // 
+            // lblMonto
+            // 
+            lblMonto.AutoSize = true;
+            lblMonto.Location = new Point(257, 76);
+            lblMonto.Name = "lblMonto";
+            lblMonto.Size = new Size(121, 20);
+            lblMonto.TabIndex = 10;
+            lblMonto.Text = "Ingrese el Monto";
+            // 
+            // lblError
+            // 
+            lblError.AutoSize = true;
+            lblError.Location = new Point(227, 160);
+            lblError.Name = "lblError";
+            lblError.Size = new Size(13, 20);
+            lblError.TabIndex = 11;
+            lblError.Text = " ";
+            // 
+            // cmnuMenu
+            // 
+            cmnuMenu.ImageScalingSize = new Size(20, 20);
+            cmnuMenu.Items.AddRange(new ToolStripItem[] { cambiarColorDeFondoToolStripMenuItem, limpiarHistorialToolStripMenuItem, salirToolStripMenuItem });
+            cmnuMenu.Name = "cmnuMenu";
+            cmnuMenu.Size = new Size(238, 76);
+            cmnuMenu.Text = "Menu";
+            // 
+            // cambiarColorDeFondoToolStripMenuItem
+            // 
+            cambiarColorDeFondoToolStripMenuItem.Name = "cambiarColorDeFondoToolStripMenuItem";
+            cambiarColorDeFondoToolStripMenuItem.Size = new Size(237, 24);
+            cambiarColorDeFondoToolStripMenuItem.Text = "Cambiar color de fondo";
+            cambiarColorDeFondoToolStripMenuItem.Click += cambiarColorDeFondoToolStripMenuItem_Click;
+            // 
+            // limpiarHistorialToolStripMenuItem
+            // 
+            limpiarHistorialToolStripMenuItem.Name = "limpiarHistorialToolStripMenuItem";
+            limpiarHistorialToolStripMenuItem.Size = new Size(237, 24);
+            limpiarHistorialToolStripMenuItem.Text = "Limpiar historial";
+            limpiarHistorialToolStripMenuItem.Click += limpiarHistorialToolStripMenuItem_Click;
+            // 
+            // salirToolStripMenuItem
+            // 
+            salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            salirToolStripMenuItem.Size = new Size(237, 24);
+            salirToolStripMenuItem.Text = "Salir";
+            salirToolStripMenuItem.Click += salirToolStripMenuItem_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(800, 450);
+            ContextMenuStrip = cmnuMenu;
+            Controls.Add(lblError);
+            Controls.Add(lblMonto);
+            Controls.Add(lblUsuario);
+            Controls.Add(lstHistorial);
+            Controls.Add(lblSaldoActual);
+            Controls.Add(txtmonto);
+            Controls.Add(txtUsuario);
+            Controls.Add(btnLimpiar);
+            Controls.Add(btnSalir);
+            Controls.Add(btnConsultar);
+            Controls.Add(btnRetirar);
+            Controls.Add(btnDepositar);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            cmnuMenu.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnDepositar;
+        private Button btnRetirar;
+        private Button btnConsultar;
+        private Button btnSalir;
+        private Button btnLimpiar;
+        private TextBox txtUsuario;
+        private TextBox txtmonto;
+        private Label lblSaldoActual;
+        private ListBox lstHistorial;
+        private Label lblUsuario;
+        private Label lblMonto;
+        private Label lblError;
+        private ContextMenuStrip cmnuMenu;
+        private ToolStripMenuItem cambiarColorDeFondoToolStripMenuItem;
+        private ToolStripMenuItem limpiarHistorialToolStripMenuItem;
+        private ToolStripMenuItem salirToolStripMenuItem;
+        private ColorDialog colorDialog1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,159 @@
+namespace Examen1CajeroAutomatico
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Para cambiar el color segun el saldo 
+        private void ColorEtiquetaSaldoActual()
+        {
+            double SaldoActual = double.Parse(lblSaldoActual.Text);
+
+            if (SaldoActual >= 30000)
+            {
+                lblSaldoActual.BackColor = Color.Green;
+            }
+
+            else if (SaldoActual >= 10000 && SaldoActual <= 30000)
+            {
+                lblSaldoActual.BackColor = Color.Yellow;
+            }
+            else
+            {
+                lblSaldoActual.BackColor = Color.Red;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            double Saldo_actual = 50000;  //definir el saldo inicial
+            lblSaldoActual.Text = Saldo_actual.ToString();  //convertir a cadena de texto
+            ColorEtiquetaSaldoActual();
+        }
+
+
+
+
+        private void btnDepositar_Click(object sender, EventArgs e)
+        {
+            string depositar_monto = txtmonto.Text; // Guarda el texto ingresado en txtMonto dentro de la variable depositar_monto.
+            double depositar;
+
+            if (double.TryParse(depositar_monto, out depositar))  //Verificar si el valor ingresado es un número válido
+            {
+                double Saldo_actual = double.Parse(lblSaldoActual.Text); //convierte el texto en un numero decimal
+                Saldo_actual += depositar; //suma el valor de depositar a saldo actual
+
+                lblSaldoActual.Text = Saldo_actual.ToString(); // convierte el # almacenado en un texto y asigna ese texto al lbl para que se muestre en la interfaz
+
+                lstHistorial.Items.Add($"Deposito {depositar} \n -Saldo Actual {Saldo_actual}"); // se agregan comentario al list box.
+
+                ColorEtiquetaSaldoActual();
+            }
+            else
+            {
+                lblError.Text = "Error! Ingrese un monto valido"; // etiqueta con error si no se cumple el if
+            }
+
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit(); // se le asigna cerrar la app
+
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtUsuario.Text = " "; // cadena de texto vacia para que se limpie cuando se hace click en el boton limpiar
+            txtmonto.Text = " ";
+            lblSaldoActual.Text = "50000";
+            lstHistorial.Items.Clear();
+            lblError.Text = "";
+            lblSaldoActual.BackColor = Color.White;
+
+        }
+
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+            double saldo_actual = double.Parse(lblSaldoActual.Text); //obtener el saldo actual desde la etiqueta
+            lstHistorial.Items.Add(txtUsuario.Text + $" Su Saldo actual es  {saldo_actual}");
+        }
+
+        private void limpiarHistorialToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           
+            lstHistorial.Items.Clear();
+            
+
+        }
+
+        private void lblSaldoActual_Click(object sender, EventArgs e)
+        {
+            double etiqueta_color = double.Parse(lblSaldoActual.Text);
+
+            if (etiqueta_color < 30000) ;
+
+        }
+
+        private void btnRetirar_Click(object sender, EventArgs e)
+        {
+
+            string monto_retirar = txtmonto.Text;  // Obtiene el texto ingresado por el usuario en la caja de texto txtmonto
+
+            double retirar;// Declara una variable para almacenar el valor numérico del monto a retirar
+
+            if (double.TryParse(monto_retirar, out retirar)) // Intenta convertir el texto ingresado a un número (double)
+            {
+                double saldo_actual = double.Parse(lblSaldoActual.Text); // Convierte el saldo actual mostrado en la etiqueta lblSaldoActual a un número
+
+                if (retirar > 0 && retirar <= saldo_actual)// Verifica que el monto a retirar sea mayor que 0 y que no exceda el saldo actual
+                {
+                    saldo_actual -= retirar;// Resta el monto retirado al saldo actual
+
+                    lblSaldoActual.Text = saldo_actual.ToString(); // Actualiza la etiqueta del saldo con el nuevo saldo
+                    lstHistorial.Items.Add($"Retiro {retirar} \n -Saldo Actual {saldo_actual}");  // Agrega el retiro al historial de transacciones
+                    lblError.Text = "";// Limpia cualquier mensaje de error anterior
+
+                    ColorEtiquetaSaldoActual();
+                }
+                else
+                {
+
+                    lblError.Text = "Error: Monto insuficiente o inválido para retirar."; // Si el monto es mayor que el saldo o es inválido (negativo o cero), muestra un error
+                }
+            }
+            else
+            {
+
+                lblError.Text = "Error: Ingrese un monto válido para retirar."; // Si el texto ingresado no es un número válido, muestra un mensaje de error
+            }
+
+
+        }
+
+        private void cambiarColorDeFondoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            if (colorDialog.ShowDialog() == DialogResult.OK) ;
+            {
+                this.BackColor = colorDialog.Color;
+
+            }
+
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit(); // se cierra desde el menuStrip
+
+        }
+
+       
+        
+
+        }
+    }
+
